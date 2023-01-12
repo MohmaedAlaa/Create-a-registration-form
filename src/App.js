de0c946b289b7fc0c333c4460ecf4279be349fc8
Mohamed Alaa <alahlybeaue@gmail.com>
2023-01-12 21:21:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import "./App.css";
+import { useState } from "react";
+import { validateEmail } from "./utils";
+
+function App() {
+
+  const PasswordErrorMessage = () => {
+    return (
+      <p className="FieldError">Password should have at least 8 characters</p>
+    );
+  };
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+
+  const [role, setRole] = useState("role");
+
+  const getIsFormValid = (e) => {
+    // Implement this function
+    if(firstName.length > 0 && validateEmail(email) && password.length >= 8 && (role === 'individual' || role === 'business'))
+    return true
+  };
+
+  const clearForm = () => {
+    // Implement this function
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setRole("role");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Account created!");
+    clearForm();
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>
+              First name <sup>*</sup>
+            </label>
+            <input placeholder="First name" type='text' value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+          </div>
+          <div className="Field">
+            <label>Last name</label>
+            <input placeholder="Last name" type='text' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+          </div>
+          <div className="Field">
+            <label>
+              Email address <sup>*</sup>
+            </label>
+            <input placeholder="Email address" type='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          </div>
+          <div className="Field">
+          <label>
+            Password <sup>*</sup>
+          </label>
+            <input
+              value={password.value}
+              type="password"
+              onChange={(e) => {
+                setPassword({ ...password, value: e.target.value });
+              }}
+              onBlur={() => {
+                setPassword({ ...password, isTouched: true });
+              }}
+              placeholder="Password"
+            />
+            {password.isTouched && password.value.length < 8 ? (
+              <PasswordErrorMessage />
+            ) : null}
+          </div>
+
+          <div className="Field">
+            <label>
+              Role <sup>*</sup>
+            </label>
+            <select value={role} onChange={(e)=>setRole(e.target.value)}>
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>
+            Create account
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
+
+export default App;
